c180427d1b7c69c13a9318a5cf02f34191bf7b11
Aaron E <aaron.evans@hashicorp.com>
2023-02-20 20:58:53 +1100

    added ssm instance profile and output

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,8 @@ output "security_group_ssh_id" {
   description = "Security Group SSH ID"
   value       = module.security_group_ssh[0].security_group_id
 }
+
+output "ssm_instance_profile" {
+  description = "Provides an IAM instance profile."
+  value       = aws_iam_instance_profile.ssm_instance_profile
+}
---
+++ ssm.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "ssm_instance_profile"
+  role = aws_iam_role.ssm_role.id
+}
+
+resource "aws_iam_role" "ssm_role" {
+  name = "ssm_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.ssm_role.name
+}
