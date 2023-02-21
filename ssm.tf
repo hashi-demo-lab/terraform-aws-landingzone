b6aee15c404360f21f283ec1646757beecd3d3fc
Aaron E <aaron.evans@hashicorp.com>
2023-02-21 11:10:28 +1100

    format

--- ssm.tf
+++ ssm.tf
@@ -1,12 +1,12 @@
 resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  count   = (var.enable_ssm ? 1 : 0)
-  name = "ssm_instance_profile"
-  role = aws_iam_role.ssm_role[0].id
+  count = (var.enable_ssm ? 1 : 0)
+  name  = "ssm_instance_profile"
+  role  = aws_iam_role.ssm_role[0].id
 }
 
 resource "aws_iam_role" "ssm_role" {
-  count   = (var.enable_ssm ? 1 : 0)
-  name = "ssm_role"
+  count = (var.enable_ssm ? 1 : 0)
+  name  = "ssm_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -22,7 +22,7 @@ resource "aws_iam_role" "ssm_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
-  count   = (var.enable_ssm ? 1 : 0)
+  count      = (var.enable_ssm ? 1 : 0)
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = aws_iam_role.ssm_role[0].name
 }
