f3ee6e49319242ffaf2a28b304e33a95e02efc4b
Aaron Evans <aarone@outlook.com.au>
2023-10-25 22:48:44 +1100

    Revert "Revert "updated""
    This reverts commit 0ff8665584f6cad45dcd18fa84dbbf593461f91b.

--- variables.tf
+++ variables.tf
@@ -1,104 +1,95 @@
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "ap-southeast-2"
 }
 
 variable "availability_zones" {
-  description = "AWS region"
-  type        = list(any)
+  description = "List of availability zones"
+  type        = list(string)
   default     = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
 }
+
 variable "owner" {
   description = "Resource owner identified using an email address"
   type        = string
+  default     = "owner@example.com"
 }
 
 variable "ttl" {
-  description = "Resource TTL (time-to-live)"
+  description = "Resource TTL (time-to-live) in days"
   type        = number
+  default     = 30
 }
 
 variable "deployment_id" {
   description = "Deployment id"
   type        = string
+  default     = "my-deployment"
 }
 
 variable "vpc_cidr" {
   description = "VPC CIDR"
   type        = string
+  default     = "10.200.0.0/16"
 }
 
 variable "public_subnets" {
-  description = "Public subnets"
-  type        = list(any)
+  description = "Public subnets CIDR blocks"
+  type        = list(string)
+  default     = ["10.200.10.0/24"]
 }
 
 variable "private_subnets" {
-  description = "Private subnets"
-  type        = list(any)
+  description = "Private subnets CIDR blocks"
+  type        = list(string)
+  default     = ["10.200.20.0/24"]
 }
 
 variable "aws_key_pair_key_name" {
   description = "AWS key pair name"
   type        = string
+  default     = "my-keypair"
 }
 
 variable "ssh_pubkey" {
-  description = "ssh public key"
+  description = "SSH public key"
   type        = string
+  default     = "ssh-rsa AAAA..."
 }
 
 variable "workspace_type" {
   description = "Terraform Cloud workspace type"
   type        = string
+  default     = "default"
 }
 
 variable "enable_vpc" {
-  description = <<EOT
-    (Optional) Enable AWS VPC creation.
-
-    Default: true
-  EOT
+  description = "Enable AWS VPC creation"
   type        = bool
   default     = true
 }
 
 variable "enable_http_access" {
-  description = <<EOT
-    (Optional) Enable http access.
-
-    Default: true
-  EOT
+  description = "Enable HTTP access"
   type        = bool
   default     = true
 }
 
 variable "enable_ssh_access" {
-  description = <<EOT
-    (Optional) Enable ssh access.
-
-    Default: true
-  EOT
+  description = "Enable SSH access"
   type        = bool
   default     = true
 }
 
 variable "enable_tgw" {
-  description = <<EOT
-    (Optional) Enable AWS Transit Gateway.
-
-    Default: true
-  EOT
+  description = "Enable AWS Transit Gateway"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "enable_ssm" {
-  description = <<EOT
-    (Optional) Enable AWS Systems Management.
-
-    Default: true
-  EOT
+  description = "Enable AWS Systems Management"
   type        = bool
   default     = true
 }
