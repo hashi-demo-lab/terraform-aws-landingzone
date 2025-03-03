cd48ec5428412e46f360e402707e9b20c5979161
Aaron Evans <aarone@outlook.com.au>
2025-03-03 13:28:28 +1100

    Comment out IAM and security group resources in main.tf for future reference

--- main.tf
+++ main.tf
@@ -31,64 +31,64 @@ module "vpc" {
   }
 }
 
-resource "aws_iam_instance_profile" "ssm_instance_profile" {
-  count = var.enable_ssm ? 1 : 0
-  name  = "ssm_instance_profile_${local.deployment_id}"
-  role  = aws_iam_role.ssm_role[0].id
-}
+# resource "aws_iam_instance_profile" "ssm_instance_profile" {
+#   count = var.enable_ssm ? 1 : 0
+#   name  = "ssm_instance_profile_${local.deployment_id}"
+#   role  = aws_iam_role.ssm_role[0].id
+# }
 
-resource "aws_iam_role" "ssm_role" {
-  count = var.enable_ssm ? 1 : 0
-  name  = "ssm_role_${local.deployment_id}"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+# resource "aws_iam_role" "ssm_role" {
+#   count = var.enable_ssm ? 1 : 0
+#   name  = "ssm_role_${local.deployment_id}"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
-  count      = var.enable_ssm ? 1 : 0
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  role       = aws_iam_role.ssm_role[0].name
-}
+# resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+#   count      = var.enable_ssm ? 1 : 0
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+#   role       = aws_iam_role.ssm_role[0].name
+# }
 
-resource "aws_key_pair" "main" {
-  key_name   = "${local.deployment_id}-${var.aws_key_pair_key_name}"
-  public_key = var.ssh_pubkey
-}
+# resource "aws_key_pair" "main" {
+#   key_name   = "${local.deployment_id}-${var.aws_key_pair_key_name}"
+#   public_key = var.ssh_pubkey
+# }
 
-module "security_group_http" {
-  count   = var.enable_http_access ? 1 : 0
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+# module "security_group_http" {
+#   count   = var.enable_http_access ? 1 : 0
+#   source  = "terraform-aws-modules/security-group/aws"
+#   version = "5.1.0"
 
-  name        = "${local.deployment_id}-http"
-  description = "Security group allowing HTTP access"
-  vpc_id      = module.vpc[0].vpc_id
+#   name        = "${local.deployment_id}-http"
+#   description = "Security group allowing HTTP access"
+#   vpc_id      = module.vpc[0].vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80"]
-}
+#   ingress_cidr_blocks = ["0.0.0.0/0"]
+#   egress_cidr_blocks  = ["0.0.0.0/0"]
+#   ingress_rules       = ["http-80"]
+# }
 
-module "security_group_ssh" {
-  count   = var.enable_ssh_access ? 1 : 0
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+# module "security_group_ssh" {
+#   count   = var.enable_ssh_access ? 1 : 0
+#   source  = "terraform-aws-modules/security-group/aws"
+#   version = "5.1.0"
 
-  name        = "${local.deployment_id}-ssh"
-  description = "Security group allowing SSH access"
-  vpc_id      = module.vpc[0].vpc_id
+#   name        = "${local.deployment_id}-ssh"
+#   description = "Security group allowing SSH access"
+#   vpc_id      = module.vpc[0].vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
-  ingress_rules       = ["ssh"] 
-}
+#   ingress_cidr_blocks = ["0.0.0.0/0"]
+#   egress_cidr_blocks  = ["0.0.0.0/0"]
+#   ingress_rules       = ["ssh"] 
+# }
